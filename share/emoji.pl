ba762e2a6d2d8b0104098cdcfa94cb590248797d
Ricardo Signes <rjbs@cpan.org>
2015-10-16 10:36:03 -0400

    add a few more emoji

--- share/emoji.pl
+++ share/emoji.pl
@@ -3,6 +3,7 @@ return {
   '+1' => "\N{THUMBS UP SIGN}",
   '-1' => "\N{THUMBS DOWN SIGN}",
 
+  'ab'                    => "\N{NEGATIVE SQUARED AB}",
   'angry'                 => "\N{ANGRY FACE}",
   'anguished'             => "\N{ANGUISHED FACE}",
   'blush'                 => "\N{SMILING FACE WITH SMILING EYES}",
@@ -19,6 +20,7 @@ return {
   'frowning'              => "\N{FROWNING FACE WITH OPEN MOUTH}",
   'grin'                  => "\N{GRINNING FACE WITH SMILING EYES}",
   'grinning'              => "\N{GRINNING FACE}",
+  'headphones'            => "\N{HEADPHONE}",
   'heart'                 => "\N{BLACK HEART SUIT}\x{FE0F}",
   'heart_eyes'            => "\N{SMILING FACE WITH HEART-SHAPED EYES}",
   'imp'                   => "\N{IMP}",
@@ -28,11 +30,16 @@ return {
   'kissing_closed_eyes'   => "\N{KISSING FACE WITH CLOSED EYES}",
   'kissing_heart'         => "\N{FACE THROWING A KISS}",
   'kissing_smiling_eyes'  => "\N{KISSING FACE WITH SMILING EYES}",
+  'monkey',               => "\N{MONKEY}",
+  'monkey_face',          => "\N{MONKEY FACE}",
+  'microphone'            => "\N{MICROPHONE}",
+  'microphone'            => "\N{MICROPHONE}",
   'neutral_face'          => "\N{NEUTRAL FACE}",
   'ok'                    => "\N{SQUARED OK}",
   'ok_hand'               => "\N{OK HAND SIGN}",
   'pensive'               => "\N{PENSIVE FACE}",
   'persevere'             => "\N{PERSEVERING FACE}",
+  'phone'                 => "\N{BLACK TELEPHONE}\x{FE0F}",
   'poop'                  => "\N{PILE OF POO}",
   'pouting_cat'           => "\N{POUTING CAT FACE}",
   'rage'                  => "\N{POUTING FACE}",
@@ -40,6 +47,7 @@ return {
   'relaxed'               => "\N{WHITE SMILING FACE}",
   'relieved'              => "\N{RELIEVED FACE}",
   'satisfied'   => "\N{SMILING FACE WITH OPEN MOUTH AND TIGHTLY-CLOSED EYES}",
+  'saxophone'             => "\N{SAXOPHONE}",
 
   # This should really be SLIGHTLY SMILING FACE, but OS X 10.11.0 and iOS 9.0
   # do not have this glyph.  Apparently it's in iOS 9.1. -- rjbs, 2015-10-12
@@ -53,6 +61,8 @@ return {
   'sunglasses'  => "\N{SMILING FACE WITH SUNGLASSES}",
   'sweat'       => "\N{FACE WITH COLD SWEAT}",
   'sweat_smile' => "\N{SMILING FACE WITH OPEN MOUTH AND COLD SWEAT}",
+  'telephone'   => "\N{BLACK TELEPHONE}\x{FE0F}",
+  'tongue'      => "\N{TONGUE}",
   'triumph'     => "\N{FACE WITH LOOK OF TRIUMPH}",
   'unamused'    => "\N{UNAMUSED FACE}",
   'wave'        => "\N{WAVING HAND SIGN}",
@@ -61,7 +71,8 @@ return {
   'worried'     => "\N{WORRIED FACE}",
   'yum'         => "\N{FACE SAVOURING DELICIOUS FOOD}",
 
-  'stuck_out_tongue' => "\N{FACE WITH STUCK-OUT TONGUE}",
+  'telephone_receiver'  => "\N{TELEPHONE RECEIVER}",
+  'stuck_out_tongue'    => "\N{FACE WITH STUCK-OUT TONGUE}",
   'stuck_out_tongue_closed_eyes' => "\N{FACE WITH STUCK-OUT TONGUE AND TIGHTLY-CLOSED EYES}",
   'stuck_out_tongue_winking_eyes' => "\N{FACE WITH STUCK-OUT TONGUE AND WINKING EYE}",
 
